bdc936fcbceb40dcad1375a784e832dc2da2eac0
Kamalesh Suthar <kamalesh.suthar.98@gmail.com>
2020-09-09 11:22:11 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route , BrowserRouter} from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PlayerSection from './PlayerSection/PlayerSection'
+import PlaylistSection from './PlaylistSection/PlaylistSection'
+
+class App extends React.Component {
+
+    state = {
+        id: 1,
+    }
+
+    changeId = (videoId) => {
+        this.setState({id : videoId})
+    }
+
+    render() {
+    return(
+        <BrowserRouter>
+            {/*<div className='App'>*/}
+            {/*    <div className='left'>*/}
+                    <Route path={"/:vid"} render={(props) => <PlayerSection {...props}/>}/>
+                {/*</div>*/}
+                {/*<div className='right'>*/}
+                    <Route path={"/:vid"} render={(props) => <PlaylistSection parentState={this.changeId} {...props}/>}/>
+            {/*    </div>*/}
+            {/*</div>*/}
+        </BrowserRouter>
+    )}
 }
 
 export default App;
--- src/VideoCard/VideoCard.js
+++ src/VideoCard/VideoCard.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 import classes from './VideoCard.module.css';
 
-const VideoCard = (props) => {
+class VideoCard extends React.Component {
 
-    return(
-        <Link className={classes.VideoCard} to={"#"}>
-            <div>
-                <img className={classes.Thumbnail} src={props.thumbnail} alt="Video Thumbnail" />
-                <h3 className={classes.Title}>{props.title}</h3>
-            </div>
-        </Link>
-    );
+    handleClick = () => {
+        this.props.history.push(`/${this.props.vidId}`)
+    }
+
+    render() {
+        console.log(this.props)
+        return(
+            <Link to={`/${this.props.vidId}`} className={classes.link}>
+                <div onClick={this.handleClick} className={`${classes.CardContainer} ${this.props.ID === this.props.vidId ? classes.Yellow : null}`} >
+                    <img className={classes.Thumbnail} src={this.props.thumbnail} alt="Video Thumbnail" />
+                    <h3 className={classes.Title}>{this.props.title}</h3>
+                </div>
+            </Link>
+        );
+    }
 }
 
 export default VideoCard;
