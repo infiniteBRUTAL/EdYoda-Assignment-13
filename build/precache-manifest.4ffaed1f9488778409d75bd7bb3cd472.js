208b7288cd2c13c104e519f9c73e152a478bdfd0
Kamalesh Suthar <kamalesh.suthar.98@gmail.com>
2020-09-24 19:48:20 +0530

    firebase version

---
+++ build/precache-manifest.4ffaed1f9488778409d75bd7bb3cd472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ebd89121de92ddcb1e890fb50d1ec0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dbda3494c414a217c83",
+    "url": "/static/css/main.44ebb53b.chunk.css"
+  },
+  {
+    "revision": "ccb106b8b0ee487ceed1",
+    "url": "/static/js/2.29aa2799.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.29aa2799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dbda3494c414a217c83",
+    "url": "/static/js/main.08436b76.chunk.js"
+  },
+  {
+    "revision": "964ec8654e04cc56add7",
+    "url": "/static/js/runtime-main.bc4cfe35.js"
+  }
+]);
